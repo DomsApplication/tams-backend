88fc6e4afcc7445727ca4b0b0af8e077f56b6bcb
Ganeshnag <ganeshnagshankar@gmail.com>
2025-05-03 15:52:11 +0530

    Logout implementation

---
+++ backend/src/main/java/com/shavika/websever/api/LogoutController.java
@@ -0,0 +1,29 @@
+package com.shavika.websever.api;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/logout")
+public class LogoutController {
+
+    @PostMapping
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        return ResponseEntity.ok("Logged out successfully");
+    }
+}
