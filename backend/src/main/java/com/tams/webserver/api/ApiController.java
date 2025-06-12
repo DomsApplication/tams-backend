37b133eb1166e1df0cd9def4019d77432df3c739
mahesha boraiah <mbmahesha47@gmail.com>
2025-06-12 08:18:07 +0530

    Modified the deployment changes

--- backend/src/main/java/com/tams/webserver/api/ApiController.java
+++ backend/src/main/java/com/tams/webserver/api/ApiController.java
@@ -17,7 +17,7 @@ public class ApiController {
 
     @GetMapping("/health")
     public ResponseEntity<HealthCheck> healthCheck() {
-        HealthCheck response = HealthCheck.builder().service_status("Service is Up.").build();
+        HealthCheck response = HealthCheck.builder().service_status("Service is Up & Running....").build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
