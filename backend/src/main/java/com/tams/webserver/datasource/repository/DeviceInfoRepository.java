5e4f0d61963d8c4cc2bda3475f71d3130d897763
Ganeshnag <ganeshnagshankar@gmail.com>
2025-06-15 23:07:58 +0530

    changes for device registeration

--- backend/src/main/java/com/tams/webserver/datasource/repository/DeviceInfoRepository.java
+++ backend/src/main/java/com/tams/webserver/datasource/repository/DeviceInfoRepository.java
@@ -2,8 +2,12 @@ package com.tams.webserver.datasource.repository;
 
 import com.tams.webserver.datasource.entity.DeviceInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,13 @@ public interface DeviceInfoRepository  extends JpaRepository<DeviceInfo, String>
 
     Optional<DeviceInfo> findByDeviceSerialNo(String deviceSerialNo);
 
+    @Modifying
+    @Query("update DeviceInfo di set di.isRegistered = true where di.cloudId = :deviceSerialNo")
+    int registerDevice(@Param("deviceSerialNo") String deviceSerialNo);
+
+    @Query("select di from DeviceInfo di where di.isRegistered = true")
+    List<DeviceInfo> findAllRegistered();
+
+    @Query("select di from DeviceInfo di where di.isRegistered = false")
+    List<DeviceInfo> findAllUnRegistered();
 }
